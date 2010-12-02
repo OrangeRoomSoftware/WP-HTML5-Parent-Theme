988afa627a2185753ee1868cd466e64d7016987d
Phill Kenoyer <phill@kenoyer.com>
2010-12-02 13:25:09 -0800

    Removed blank description/keywords.

--- header.php
+++ header.php
@@ -4,17 +4,15 @@
  * @subpackage HTML5_Boilerplate
  */
 ?><!doctype html>
-<html lang="en" class="no-js">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <!--[if IE]><![endif]-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-    <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-    <meta name="description" content="">
-    <meta name="author" content="">
     <link rel="shortcut icon" href="<?php echo ORS_TEMPLATE_URL; ?>/images/favicon.ico">
     <link rel="apple-touch-icon" href="<?php echo ORS_TEMPLATE_URL; ?>/images/apple-touch-icon.png">
+    <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
