c5a66b6d912a9ba64048ee63d813e9f7b540cf1a
Phill Kenoyer <phill@kenoyer.com>
2011-03-17 14:50:57 -0700

    Clean up.

--- header.php
+++ header.php
@@ -7,11 +7,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <!--[if IE]><![endif]-->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
-    <link rel="shortcut icon" href="<?php echo ORS_TEMPLATE_URL; ?>/images/favicon.ico">
-    <link rel="apple-touch-icon" href="<?php echo ORS_TEMPLATE_URL; ?>/images/apple-touch-icon.png">
+    <?php if (file_exists(ORS_TEMPLATE_DIR.'/images/favicon.ico')) { ?><link rel="shortcut icon" href="<?php echo ORS_TEMPLATE_URL; ?>/images/favicon.ico"><?php } ?>
     <title><?php wp_title('-', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
   </head>
