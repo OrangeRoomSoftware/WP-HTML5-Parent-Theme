1eb2737e55d503ee71a3c45e5f01f814f55a31a2
Phill Kenoyer <phill@kenoyer.com>
2011-07-10 14:01:53 -0700

    Fixed missing class

--- 404.php
+++ 404.php
@@ -5,16 +5,14 @@
  */
 
 get_header(); ?>
-<section id="main" role="main" class="container_12">
-  <article <?php post_class('grid_12') ?> id="404-not-found">
-    <header>
-      <h1>Not Found</h1>
-    </header>
+<div id="main" role="main" class="container_12">
+  <article id="404-not-found" class='grid_12'>
     <section>
+      <h2>Not Found</h2>
       <p>Sorry, but you are looking for something that isn't here.</p>
       <?php get_search_form(); ?>
     </section>
   </article>
-</section>
+</div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
